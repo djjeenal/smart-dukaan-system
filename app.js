12f6572c520a9372ec69630049c7683a043348b4
djjeenal <91907170+djjeenal@users.noreply.github.com>
2026-01-09 11:30:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+document.querySelector(".user").onclick = () => {
+  alert("Customer flow will start here");
+};
+
+document.querySelector(".admin").onclick = () => {
+  alert("Admin login will start here");
+};
+
+document.querySelector(".super").onclick = () => {
+  alert("Super Admin panel will start here");
+};
